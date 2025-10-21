3c9754f43d57530d6ae9b769dd7929ba39f9741e
oBeta97 <paolopellizzari97@gmail.com>
2025-10-21 20:20:48 +0200

    main - esercizio 3

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
 const app = Vue.createApp({
     data() {
         return {
-            output1: '',
-            output2: '',
-            partialInput: ''
+            counter: 0,
+            isTimerRunning: false,
+            timerId: 0
         };
     },
-    computed:{
-        test(){
-            return this.asdf(this.output1);
+    watch: {
+        isTimerRunning(value) {
+            if (value && this.timerId === 0) {
+                console.log('Starting timer...');
+                this.timerId = setTimeout(() => {
+                    if(this.counter !== 37){
+                        this.counter = 0;
+                        alert('The time is over, you lost!');
+                        return;
+                    }
+
+                    alert('Congratulations, you won!');
+                }, 5000);
+            } 
+        }
+    },
+    computed: {
+        result() {
+
+            switch(true){
+                case (this.counter === 37):
+                    return 'You did it!!';
+                case this.counter === 0:
+                    return 'Start playing!';
+                case (this.counter < 37):
+                    return 'Not there yet';
+                case (this.counter > 37):
+                    return 'Too much!';
+            }
+
+            return this.counter;
         }
     },
     methods: {
-        asdf(input){
-            return 'ciao ' + input;
+        add5() {
+            this.counter += 5;
+            this.startTimer();
         },
-        showAlert() {
-            alert('Button was pressed!');
+        add1() {
+            this.counter += 1;
+            this.startTimer();
         },
-        updateOutput2(event) {
-            this.output2 = this.partialInput;
-        },
-        updatePartialInput(event) {
-            this.partialInput = event.target.value;
+        startTimer() {
+            if(this.isTimerRunning){
+                return;
+            }
+            
+            console.log('Starting timer from method...');
+            this.isTimerRunning = true;
         }
     }
 });

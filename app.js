f7cbb760ed6d12fedfed7797b610e1cc12f5ee2d
oBeta97 <paolopellizzari97@gmail.com>
2025-10-21 19:07:37 +0200

    main - esercizio 2

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            countNumber: 0,
-            defaultText: 'This is a funny cat gif!',
-            enteredText: 'empty',
-            randomCat: 'https://cataas.com/cat/gif?width=300'
-        }
+            output1: '',
+            output2: '',
+            partialInput: ''
+        };
     },
     methods: {
-        plus() {
-            this.countNumber++;
+        showAlert() {
+            alert('Button was pressed!');
         },
-        plusVar(num){
-            this.countNumber += num;
+        updateOutput1(event) {
+            this.output1 = event.target.value;
         },
-        plusRandom() {
-            this.countNumber += Math.floor(Math.random() * 10) + 1;
+        updateOutput2(event) {
+            this.output2 = this.partialInput;
         },
-        less() {
-            this.countNumber--;
-        },
-        lessVar(num){
-            this.countNumber -= num;
-        },
-        lessRandom() {
-            this.countNumber -= Math.floor(Math.random() * 10) + 1;
-        },
-        reset() {
-            this.countNumber = 0;
-        },
-        updateText(event, baseText) {
-            if(event.target.value.length !== 0)
-                this.enteredText = baseText + event.target.value;
-            else
-                this.enteredText = 'empty';
+        updatePartialInput(event) {
+            this.partialInput = event.target.value;
         }
     }
 });

6522a47a99368e65c760a006de23e2d22627a0d8
oBeta97 <paolopellizzari97@gmail.com>
2025-10-24 20:35:25 +0200

    main - esercizio4 dynamic styling

--- app.js
+++ app.js
@@ -1,55 +1,54 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0,
-            isTimerRunning: false,
+            // for style me paragraphs
+            enterdClass: '',
+            enterdClass2: '',
+            isPVisible: true,
+            pBgColor: '',
+
+            // for jhonny style
+            jStyle: '',
+            jView: true
         };
     },
-    watch: {
-        result() {
-            if (!this.isTimerRunning) {
-                console.log('Starting timer...');
-                this.timerId = setTimeout(() => {
-                    if(this.counter !== 37){
-                        this.counter = 0;
-                        alert('The time is over, you lost!');
-                        return;
-                    }
-
-                    alert('Congratulations, you won!');
-                }, 5000);
-                this.startTimer();
-            } 
-        }
-    },
     computed: {
-        result() {
-            switch(true){
-                case (this.counter === 37):
-                    return 'You did it!!';
-                case this.counter === 0:
-                    return 'Start playing!';
-                case (this.counter < 37):
-                    return 'Not there yet';
-                case (this.counter > 37):
-                    return 'Too much!';
-            }
-
-            return this.counter;
+        // style method
+        pStyles() {
+            return {
+                visibility: this.isPVisible ? 'visible' : 'hidden',
+                backgroundColor: this.pBgColor
+            };
+        },
+        // class method
+        jClasses() {
+            return {
+                user1: this.isUser1,
+                user2: this.isUser2,
+                hidden: this.isHidden,
+                visible: this.isVisible
+            };
+        },
+        isUser1() {
+            return this.jStyle === 'user1';
+        },
+        isUser2() {
+            return this.jStyle === 'user2';
+        },
+        isVisible() {
+            return this.jView;
+        },
+        isHidden() {
+            return !this.jView;
         }
     },
     methods: {
-        addToResult(num) {
-            this.counter += num;
+        // toggle method with buttons click
+        toggleHiddenP() {
+            this.isPVisible = !this.isPVisible;
         },
-        startTimer() {
-            if(this.isTimerRunning){
-                console.log('Timer already running!');
-                return;
-            }
-            
-            console.log('Starting timer from method...');
-            this.isTimerRunning = true;
+        toggleJView() {
+            this.jView = !this.jView;
         }
     }
 });

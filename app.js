e3f484ec7eeb1fe4974b4a053b59f849d4dfc6e3
oBeta97 <paolopellizzari97@gmail.com>
2025-10-25 15:21:57 +0200

    main - v-if e v-else

--- app.js
+++ app.js
@@ -1,56 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            // for style me paragraphs
-            enterdClass: '',
-            enterdClass2: '',
-            isPVisible: true,
-            pBgColor: '',
-
-            // for jhonny style
-            jStyle: '',
-            jView: true
+            goals: [],
+            inputGoalValue: ''
         };
     },
-    computed: {
-        // style method
-        pStyles() {
-            return {
-                visibility: this.isPVisible ? 'visible' : 'hidden',
-                backgroundColor: this.pBgColor
-            };
-        },
-        // class method
-        jClasses() {
-            return {
-                user1: this.isUser1,
-                user2: this.isUser2,
-                hidden: this.isHidden,
-                visible: this.isVisible
-            };
-        },
-        isUser1() {
-            return this.jStyle === 'user1';
-        },
-        isUser2() {
-            return this.jStyle === 'user2';
-        },
-        isVisible() {
-            return this.jView;
-        },
-        isHidden() {
-            return !this.jView;
-        }
-    },
     methods: {
-        // toggle method with buttons click
-        toggleHiddenP() {
-            this.isPVisible = !this.isPVisible;
-        },
-        toggleJView() {
-            this.jView = !this.jView;
+        addGoal() {
+            if (this.inputGoalValue.trim() !== '') {
+                this.goals.push(this.inputGoalValue);
+                this.inputGoalValue = '';
+            }
         }
     }
 });
 
-app.mount('#assignment');
+app.mount('#user-goals');

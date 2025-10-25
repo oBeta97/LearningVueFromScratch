985cf2827ae60a8d68fe371374832cf38e843d9b
oBeta97 <paolopellizzari97@gmail.com>
2025-10-25 15:59:54 +0200

    main - key applicato ai for

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const app = Vue.createApp({
                 this.goals.push(this.inputGoalValue);
                 this.inputGoalValue = '';
             }
+        },
+        removeGoal(index) {
+            this.goals.splice(index, 1);
         }
     }
 });

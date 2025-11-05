0a259a784901fce2dd609675105aedb638a45720
oBeta97 <paolopellizzari97@gmail.com>
2025-11-05 23:12:37 +0100

    main - emit events e primi passi nelle composition api

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,12 +1,40 @@
 <template>
-    <p>{{ friend.name }} - {{ friend.phone }}</p>
+    <span v-if="friend !== null">{{ friend.name }} - {{ friend.phone }}</span>
+    <span v-else>{{ friendName }} - {{ phone }}</span>
+    | <button @click="selectFriend">select</button>
 </template>
 
-<script>
+<!-- Utilizzando setup entriamo su vue3 e quindi non dobbiamo esportare nulla -->
+<script setup>
+    const props = defineProps({
+        friend: {
+            type: Object,
+            required: false,
+            default: null
+        },
+        friendName:{
+            type: String,
+            required: false,
+            default: 'Unknown'
+        },
+        phone:{
+            type: String,
+            required: false,
+            default: 'N/A',
+            validator: function(value){
+                const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+                return phoneRegex.test(value);
+            }
+        }
+    });
 
-export default {
-    name : "FriendContact",
-    props: ['friend']
-}
+    // Definizione dei vari "eventi" che il componente può emettere
+    const emit = defineEmits(['friendSelected']);
 
+    // Questa è un methods! con vue3 setup non serve più definire i vari componenti
+    function selectFriend(){
+        // Emissione dell'evento 'friendSelected' con il nome dell'amico selezionato
+        emit('friendSelected', props.friend ? props.friend.name : props.friendName);
+    }
+    
 </script>
--- src/components/MyFriendList.vue
+++ src/components/MyFriendList.vue
@@ -11,9 +11,16 @@ export default {
             friends: [
                 { name: "Alice", phone: "123-456-7890" },
                 { name: "Bob", phone: "987-654-3210" },
-                { name: "Charlie", phone: "555-555-5555" }
-            ]
+                { name: "Charlie", phone: "555-555-5555" },
+                { name: "Charlie" }
+            ],
+            selectedFriend: null
         };
+    },
+    methods: {
+        friendSelected(friendName) {
+            this.selectedFriend = friendName;
+        }
     }
 }
 </script>
@@ -21,9 +28,13 @@ export default {
 <template>
     <section>
         <h2>MyFriends</h2>
+        <h4 v-if="selectedFriend !== null"> My favourite friend is: {{ this.selectedFriend }} </h4>
         <ul>
             <li v-for="(friend, index) in friends" :key="index">
-                <FriendContact :friend="friend" />
+                <FriendContact :friend="friend" @friendSelected="friendSelected" />
+            </li> 
+            <li style="list-style-type: kannada;" v-for="(friend, index) in friends" :key="index">
+                <FriendContact :friendName="friend.name" :phone="friend.phone" @friendSelected="friendSelected" />
             </li>
         </ul>
     </section>
